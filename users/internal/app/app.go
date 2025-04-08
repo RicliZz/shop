cccf235ca3bd1123d6eb7b9b01500a08135b14bf
ricliz <ricliz@mail.ru>
2025-04-08 21:46:26 +0300

    conf and start serv

---
+++ users/internal/app/app.go
@@ -0,0 +1,37 @@
+package app
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func NewServer(addr string, router *gin.Engine) *Server {
+	return &Server{
+		httpServer: &http.Server{
+			Addr:           addr,
+			Handler:        router,
+			ReadTimeout:    10 * time.Second,
+			WriteTimeout:   10 * time.Second,
+			MaxHeaderBytes: 1 << 20,
+		},
+	}
+}
+
+func Run() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	router := gin.Default()
+	router.Group("api/v1")
+	serv := NewServer(os.Getenv("AddrServ"), router)
+	serv.httpServer.ListenAndServe()
+}
